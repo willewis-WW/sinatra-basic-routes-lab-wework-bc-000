af94854d6de7a3687a75b6104e081a666525bde1
William Lewis <william.lewis@wework.com>
2018-02-27 11:33:17 +0000

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,19 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+  get '/' do
+    'Hello World!'
+  end
+  
+  get '/name' do
+    'My name is William'
+  end
+
+  get '/hometown' do
+    'My hometown is Angleton, TX'
+  end
+  
+  get '/favorite-song' do
+    'My favorite song is Yellow Submarine'
+  end
 end

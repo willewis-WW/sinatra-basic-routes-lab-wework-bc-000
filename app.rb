4b28b18fd6430bc1c66a054b7fad84d4e2d0aa8a
William Lewis <william.lewis@wework.com>
2018-02-27 11:32:15 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -10,8 +10,10 @@ class App < Sinatra::Base
   end
 
   get '/hometown' do
+    'My hometown is Angleton, TX'
   end
   
   get '/favorite-song' do
+    'My favorite song is Yellow Submarine'
   end
 end
